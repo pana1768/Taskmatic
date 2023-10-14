787e9debae4e2d86c340acda788982a5fea3de1e
Qquiqlerr <leha.metlushko@bk.ru>
2023-10-14 15:29:19 +0300

    удалил соня чета сделала не понял

--- bot/buttons/buttons.py
+++ bot/buttons/buttons.py
@@ -3,7 +3,7 @@ from telebot import types
 choosepoint_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 sozdatgr_1 = types.KeyboardButton('Группы')
 mygroups_1 = types.KeyboardButton('Задания')
-chooseactioon_markup.add(sozdatgr_1, mygroups_1)
+choosepoint_markup.add(sozdatgr_1, mygroups_1)
 
 chooserole_markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #это надо после кнопок "Мои группы", "Создать группу", "Задания"
 rukovoditel_2 = types.KeyboardButton('Я руководитель')
@@ -16,6 +16,5 @@ actgr1 = types.KeyboardButton('Мои группы')
 actgr2 = types.KeyboardButton('Создать группу')
 chooseaction_markup.add(actgr1, actgr2)
 
-chooserole_markup.add(rukovoditel_2, rabotnik_2)
 
 
