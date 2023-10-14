994e396b360bd534eaef83f10357b0c36e816faf
pana1768 <80035844+pana1768@users.noreply.github.com>
2023-10-14 14:39:11 +0300

    db

---
+++ db/model.py
@@ -0,0 +1,49 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+# from sqlalchemy import Table, Column, Integer, String, MetaData
+
+# meta_obj = MetaData()
+Base = declarative_base()
+
+class Users (Base):
+    __tablename__ = 'registered_users'
+
+    user_id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String)
+
+    def __str__(self):
+        return f'{self.user_id}: {self.name}'
+
+class Tasks(Base):
+    __tablename__ = 'tasks'
+
+    task_id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
+    name_task = sq.Column(sq.String(length=40))
+    description_task = sq.Column(sq.String(length=1000))
+    user_take = sq.Column(sq.Integer)
+
+    user = relationship(Users, backref='task')
+    def __str__(self):
+        return f'{self.task_id} : {self.name_task} : {self.description_task}'
+
+class AllGroup(Base):
+    __tablename__ = 'all_group'
+
+    group_id = sq.Column(sq.Integer,primary_key=True)
+    group_name = sq.Column(sq.String, unique=True)
+    admin_id = sq.Column(sq.Integer, unique=True)
+
+class GroupExecutor(Base):
+    __tablename__ = 'group_executor'
+
+    user_id = sq.Column(sq.Integer)
+    group_id = sq.Column(sq.Integer)
+    group_name = sq.Column(sq.String)
+
+    user = relationship(Users, backref='groupcutor')
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+
+
+
