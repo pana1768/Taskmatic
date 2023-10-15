430ef3ad68607367dbaf75a842172f4487b210a2
Sofiia Sidorenko <sonasidorenko0@gmail.com>
2023-10-15 10:05:55 +0300

    ввмвмацр

--- bot/bot.py
+++ bot/bot.py
@@ -207,7 +207,7 @@ def main():
         elif message.text == 'Свободные':
             pass
         elif message.text == 'В процессе':
-            
+
         else:
             bot.set_state(message.from_user.id, states.Groups.chooserole)
             bot.send_message(message.chat.id, "Выберите роль",reply_markup=buttons.chooserole_markup)
